065912d1c53953c5f7fd8c5d5c1a20f4480be1e4
Brolloks <94197082+Brolloks@users.noreply.github.com>
2024-07-18 17:59:29 +0100

    RCADA Error checking Updates to reg-pool.sh
    Added some fault checking and step-by-step prompts to help with catching any issues along the way

--- reg-pool.sh
+++ reg-pool.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Function to check for errors
+check_error() {
+    if [ $? -ne 0 ]; then
+        echo "An error occurred: $1"
+        read -p "Do you want to continue despite the error? (y/n): " continue_script
+        if [ "$continue_script" != "y" ]; then
+            echo "Exiting..."
+            exit 1
+        fi
+    fi
+}
+
+# Function for logging and prompting
+log_and_prompt() {
+    echo " "
+    echo "$1"
+    read -p "Press any key to continue..."
+    echo "*****************************************"
+
 network=${1:-sanchonet}
 era=${2:-conway}
 
@@ -18,6 +37,15 @@ elif [ "$network" == "prime" ]; then magic=3311 bc=apex;
 elif [ "$network" == "vector" ]; then magic=1177 bc=apex;
 else echo "Invalid network"; exit; fi
 
+echo "#####################"
+echo " "
+echo "Network: $network"
+echo "Era: $era"
+echo "Magic: $magic"
+echo "Blockchain: $bc"
+echo " "
+echo "#####################"
+
 mkdir -p ~/${network}
 echo "Ensure ~/${network} contains payment_${network}.skey, stake_${network}.skey and cold.skey."
 read -p "This will overwrite existing keys! Press Ctrl+C to cancel..."
@@ -27,18 +55,25 @@ $binCardanoCli $era node key-gen \
     --cold-signing-key-file ~/${network}/cold.skey \
     --operational-certificate-issue-counter-file ~/${network}/opcert.counter
 
+log_and_prompt "Generating node cold vkey & skeys"
 $binCardanoCli $era node key-gen-KES \
     --verification-key-file ~/${network}/kes.vkey \
     --signing-key-file ~/${network}/kes.skey
+check_error "Failed to generate cold keys."    
 
+log_and_prompt "Generating node KES vkey & skeys"
 $binCardanoCli $era node key-gen-VRF \
     --verification-key-file ~/${network}/vrf.vkey \
     --signing-key-file ~/${network}/vrf.skey
+check_error "Failed to generate KES keys."
 
+log_and_prompt "Generating Pool metadata hash file"
 $binCardanoCli $era stake-pool metadata-hash \
     --pool-metadata-file ~/${network}/poolMetadata${network^}.json \
     --out-file ~/${network}/poolMetadataHash${network^}.txt
+check_error "Failed to generate metadata hash."
 
+log_and_prompt "Creating stake-pool registration-certificate"
 $binCardanoCli $era stake-pool registration-certificate \
     --cold-verification-key-file ~/${network}/cold.vkey \
     --vrf-verification-key-file ~/${network}/vrf.vkey \
@@ -53,14 +88,22 @@ $binCardanoCli $era stake-pool registration-certificate \
     --metadata-url ${metadataUrl} \
     --metadata-hash $(cat ~/${network}/poolMetadataHash${network^}.txt) \
     --out-file ~/${network}/pool-registration.cert
+check_error "Failed to create pool registration certificate."
 
+log_and_prompt "Creating stake-pool delegation-certificate"
 $binCardanoCli $era stake-address stake-delegation-certificate \
     --stake-verification-key-file ~/${network}/stake_${network}.vkey \
     --cold-verification-key-file ~/${network}/cold.vkey \
     --out-file ~/${network}/delegation.cert
+check_error "Failed to create delegation certificate."
 
 paymentAddr=$(cat ~/${network}/payment_${network}.addr)
+echo "Payment address: $paymentAddr"
+
 utxos=$(sudo -E $binCardanoCli query utxo  --socket-path /var/lib/${bc}/${network}/node.socket --address ${paymentAddr} --testnet-magic ${magic} --out-file  /dev/stdout | jq -r 'keys[0]')
+check_error "Failed to query UTXOs."
+
+log_and_prompt "Building transaction"
 sudo -E $binCardanoCli $era transaction build \
     --testnet-magic ${magic} \
     --socket-path /var/lib/${bc}/${network}/node.socket \
@@ -70,7 +113,9 @@ sudo -E $binCardanoCli $era transaction build \
     --certificate-file ~/${network}/pool-registration.cert \
     --certificate-file ~/${network}/delegation.cert \
     --out-file ~/${network}/tx.raw
+check_error "Failed to build transaction."
 
+log_and_prompt "Signing transaction"
 sudo -E $binCardanoCli $era transaction sign \
     --tx-body-file ~/${network}/tx.raw \
     --signing-key-file ~/${network}/payment_${network}.skey \
@@ -78,37 +123,50 @@ sudo -E $binCardanoCli $era transaction sign \
     --signing-key-file ~/${network}/stake_${network}.skey \
     --testnet-magic ${magic} \
     --out-file ~/${network}/tx.signed
+check_error "Failed to sign transaction."
 
+log_and_prompt "Submitting transaction"
 sudo -E $binCardanoCli $era transaction submit \
     --testnet-magic ${magic} \
     --socket-path /var/lib/${bc}/${network}/node.socket \
     --tx-file ~/${network}/tx.signed
 sudo rm ~/${network}/tx.{raw,signed}
+check_error "Failed to submit transaction."
 
+log_and_prompt "Generating stake-pool ID Bech32 format"
 sudo -E $binCardanoCli $era stake-pool id \
     --cold-verification-key-file ~/${network}/cold.vkey \
     --output-format bech32 \
     --out-file ~/${network}/stakepool.${network}.bech32
+check_error "Failed to get stake pool id in bech32 format."
 echo "Bech32: `sudo cat stakepool.${network}.bech32`"
 
+log_and_prompt "Generating stake-pool ID HEX format"
 sudo -E $binCardanoCli $era stake-pool id \
     --cold-verification-key-file ~/${network}/cold.vkey \
     --output-format hex \
     --out-file ~/${network}/stakepool.${network}.hex
+check_error "Failed to get kesPeriod."    
 echo "Hex: `sudo cat stakepool.${network}.hex`"
 
 sudo chown `whoami`: ~/${network}/stakepool.${network}.bech32
 sudo chown `whoami`: ~/${network}/stakepool.${network}.hex
 
+log_and_prompt "Checking kesPeriod info"
 slotsPerKESPeriod=$(cat /etc/${bc}/${network}/shelley-genesis.json | jq -r '.slotsPerKESPeriod')
 slotNo=$(sudo -E $binCardanoCli query tip  --socket-path /var/lib/${bc}/${network}/node.socket --testnet-magic ${magic} | jq -r '.slot')
 kesPeriod=$((${slotNo} / ${slotsPerKESPeriod}))
+check_error "Failed to get kesPeriod."
+echo "kesPeriod: $kesPeriod"
+
+log_and_prompt "Issueing node-op-cert"
 $binCardanoCli $era node issue-op-cert \
     --kes-verification-key-file ~/${network}/kes.vkey \
     --cold-signing-key-file ~/${network}/cold.skey \
     --operational-certificate-issue-counter-file ~/${network}/opcert.counter \
     --kes-period ${kesPeriod} \
     --out-file ~/${network}/node.cert
+check_error "Failed to issue node operational certificate."
 echo "Node certificate issued."
 
 sudo mkdir -p /etc/cardano/${network}/keys
